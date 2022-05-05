6a5b2c68ed0a808581e0e88b3f32c1317e394bb2
Martin Julian <69302977+mjcaniceto@users.noreply.github.com>
2022-05-05 20:26:34 +0800

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,17 @@
+import random
+
 def primary():
-  print("I am the hunter!")
+  #print("I am the hunter!")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  print(quotes)
+  print(quotes[13])
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
